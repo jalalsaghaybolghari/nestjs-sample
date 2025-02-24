3087afc2951aeade288dd62bf439e32655ca08be
Jalal SaghayBolghari <jalalsaghaybolghari@gmail.com>
2025-02-24 12:02:37 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-const port = 80; // You can change this to any available port
+const port = 8080; // You can change this to any available port
 await app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });}
